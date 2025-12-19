2e73877065c38f605b14030b373da495e231a7fc
Hafsah Anibaba <60625866+hafsah2020@users.noreply.github.com>
2025-12-19 11:23:02 +0100

    Update script.js

--- web_maps/supermarkets/static/script.js
+++ web_maps/supermarkets/static/script.js
@@ -8,7 +8,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // --- Office marker (blue) ---
 const officeIcon = L.icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // blue office
   iconSize: [32, 32],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32]
@@ -24,41 +24,53 @@ L.marker([9.090342, 7.4312068], { icon: officeIcon })
     `;
   });
 
-// --- Supermarket icon (blue shopping cart) ---
-const supermarketIcon = L.icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/283/283182.png",
-  iconSize: [30, 30],
-  iconAnchor: [15, 30],
-  popupAnchor: [0, -30]
-});
+// --- Function to select marker icon based on stage ---
+function getStageIcon(stage) {
+  let url;
+  switch(stage.toLowerCase()) {
+    case "contacted":
+      url = "https://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+      break;
+    case "negotiating":
+      url = "https://maps.google.com/mapfiles/ms/icons/orange-dot.png";
+      break;
+    case "signed":
+      url = "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
+      break;
+    default:
+      url = "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+  }
+  return L.icon({
+    iconUrl: url,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  });
+}
 
 // --- Load Google Sheet CSV (only ID and Stage) ---
-Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRQByBMhYI-GqeivceUYqdxpYqeGNJS61_a1OTCzjaVBnEoqTfNL9eTT-ysoBytutaebSc24Swu5gUZ/pub?gid=0&single=true&output=csv", {
+Papa.parse("https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/pub?output=csv", {
   download: true,
   header: true,
   complete: function(sheetData) {
-    // Map IDs to Stage only
     const stageLookup = {};
     sheetData.data.forEach(row => {
-      if (row.ID && row.Stage) {
-        stageLookup[row.ID] = row.Stage;
-      }
+      if (row.ID && row.Stage) stageLookup[row.ID] = row.Stage;
     });
 
-    // --- Load GeoJSON ---
+    // Load GeoJSON
     fetch("static/data/supermarkets_in_abuja.geojson")
-      .then(res => {
-        if (!res.ok) throw new Error("GeoJSON not found: " + res.status);
-        return res.json();
-      })
+      .then(res => res.json())
       .then(geoData => {
         L.geoJSON(geoData, {
-          pointToLayer: (feature, latlng) => L.marker(latlng, { icon: supermarketIcon }),
+          pointToLayer: (feature, latlng) => {
+            const stage = stageLookup[feature.properties.ID] || "N/A";
+            return L.marker(latlng, { icon: getStageIcon(stage) });
+          },
           onEachFeature: (feature, layer) => {
             layer.on("click", () => {
               const props = feature.properties;
-              const stage = stageLookup[props.ID] || "N/A"; // lookup stage by ID
-              
+              const stage = stageLookup[props.ID] || "N/A";
               document.getElementById("info").innerHTML = `
                 <p><span class="info-label">Supermarket:</span> ${props.super_market || "N/A"}</p>
                 <p><span class="info-label">Phone:</span> ${props.phone_number || "N/A"}</p>
